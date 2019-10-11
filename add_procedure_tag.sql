15b258e0dffc8faa5f4d86da1414609d4d164cfc
decoejz <decoejz@gmail.com>
2019-10-11 14:56:13 -0300

    bug fix

--- add_procedure_tag.sql
+++ add_procedure_tag.sql
@@ -1,10 +1,10 @@
 DROP PROCEDURE IF EXISTS add_tag_usuario;
 
 DELIMITER //
-CREATE PROCEDURE add_tag_usuario(IN email VARCHAR(40), IN id INT)
+CREATE PROCEDURE add_tag_usuario(IN var_email VARCHAR(40), IN var_id INT)
 BEGIN
-    IF (email IN (SELECT * FROM usuario)) THEN
-		INSERT INTO tag_usuario (email, id) VALUES (email, id);
-	END IF;
+	IF (SELECT IF((SELECT COUNT(email) FROM usuario WHERE email=var_email)>0, 1,0) = 1) THEN
+		INSERT INTO tag_usuario (email, id) VALUES (var_email, var_id);
+    END IF;
 END//
 DELIMITER ;
