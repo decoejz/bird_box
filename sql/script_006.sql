af211b76e12190758652cdf5dbc96b479613b007
decoejz <decoejz@gmail.com>
2019-10-17 15:07:20 -0300

    bug fixes

--- sql/script_006.sql
+++ sql/script_006.sql
@@ -1,15 +1,13 @@
 USE bird_box;
 
-DROP TEMPORARY TABLE IF EXISTS max_vius_cid;
-CREATE TEMPORARY TABLE max_vius_cid
-	SELECT usuario.nome, usuario.email, usuario.cidade, COUNT(usuario_viu.id) as cnt_vius 
-		FROM usuario 
-		INNER JOIN post USING (email)
-		INNER JOIN usuario_viu USING (id)
-		GROUP BY usuario.cidade;
-
-DROP VIEW pop_cidade AS
+DROP VIEW IF EXISTS pop_cidade;
+CREATE VIEW pop_cidade AS
     SELECT mv.nome, mv.email, mv.cidade, MAX(mv.cnt_vius)
-    FROM max_vius_cid mv
+    FROM (
+        SELECT usuario.nome, usuario.email, usuario.cidade, COUNT(usuario_viu.id) as cnt_vius 
+        FROM usuario 
+        INNER JOIN post USING (email)
+        INNER JOIN usuario_viu USING (id)
+        GROUP BY usuario_viu.id) AS mv
     GROUP BY mv.cidade
     ORDER BY mv.cidade;
--- sql/script_007.sql
+++ sql/script_007.sql
@@ -1,19 +1,31 @@
 USE bird_box;
 
-INSERT INTO usuario (nome, email, cidade) VALUES ("andre", "decoejz", "sp"), ("manu", "manuemmail", "campinas"), ("david", "davide@mail.com", "sao jose");
+INSERT INTO usuario (nome, email, cidade) VALUES ("andre", "decoejz", "sp");
+INSERT INTO usuario (nome, email, cidade) VALUES ("manu", "manuemmail", "campinas");
+INSERT INTO usuario (nome, email, cidade) VALUES ("david", "davide@mail.com", "sao jose");
+INSERT INTO usuario (nome, email, cidade) VALUES ("usuario1", "use1@mail.com", "rj");
+INSERT INTO usuario (nome, email, cidade) VALUES ("usuario2", "use2@mail.com", "sp");
+INSERT INTO usuario (nome, email, cidade) VALUES ("usuario3", "use3@mail.com", "campinas");
+INSERT INTO usuario (nome, email, cidade) VALUES ("usuario4", "use4@mail.com", "rj");
+INSERT INTO usuario (nome, email, cidade) VALUES ("usuario5", "use5@mail.com", "sao jose");
+INSERT INTO usuario (nome, email, cidade) VALUES ("usuario6", "use6@mail.com", "sao jose");
 
-INSERT INTO preferencia (email, nome) VALUES ("decoejz", "passaro1"), ("decoejz", "pas2"), ("manuemmail","passa3");
+INSERT INTO passaro (nome) VALUES ("canario"),("beija-flor"),("sabia");
 
-INSERT INTO preferencia (email, nome) VALUES ("manuemmail","pas2");
+INSERT INTO preferencia (email, nome) VALUES ("decoejz", "passaro1"), ("decoejz", "pas2"), ("use2@mail.com","beija flor"), ("manuemmail","passa3");
+INSERT INTO preferencia (email, nome) VALUES ("manuemmail","pas2"),("use5@mail.com","sabia");
 
-INSERT INTO post (titulo, email) VALUES ("Era uma @decoejz avez...", "manuemmail");
+INSERT INTO post (titulo, texto,email, url) VALUES ("Historinha","Era uma @decoejz avez...","manuemmail","google.img/canario");
+INSERT INTO post (titulo, texto,email, url) VALUES ("Tardis","Era uma @use3@mail.com um passaro chamado #beija-flor que gosta de voar.","use1@mail.com","linkedin.passaros.com");
+INSERT INTO post (titulo, texto,email, url) VALUES ("Um pais da europa","Tirou o @davide@mail.com na copa dos #sabia","use3@mail.com","facebook.copinha.org.br");
 
-INSERT INTO usuario_viu (email,id,so,ip,browser) VALUES ("ayres@linkadinho.inspermon.br",1,"WINDOWS","192.168.1.1","Internet Explorer");
+INSERT INTO usuario_viu (email,id,so,ip,browser) VALUES ("davide@mail.com",1,"WINDOWS","192.168.1.1","Chorme");
+INSERT INTO usuario_viu (email,id,so,ip,browser) VALUES ("decoejz",2,"UBUNTU","1.2.3.4","Internet Explorer");
+INSERT INTO usuario_viu (email,id,so,ip,browser,liked) VALUES ("manuemmail",2,"WINDOWS","18.18.18.18","Internet Explorer","curtiu");
+INSERT INTO usuario_viu (email,id,so,ip,browser,liked) VALUES ("use6@mail.com",2,"MACOS","192.168.0.1","Firefox","curtiu");
+INSERT INTO usuario_viu (email,id,so,ip,browser,liked) VALUES ("use1@mail.com",2,"WINDOWS","19.16.1.1","Internet Explorer","nao curtiu");
+INSERT INTO usuario_viu (email,id,so,ip,browser,liked) VALUES ("use4@mail.com",3,"MACOS","19.0.1.1","Safari","nao curtiu");
 
-INSERT INTO usuario_viu (email,id,so,ip,browser) VALUES ("decoejz",1,"WINDOWS","192.168.1.1","Internet Explorer");
+INSERT INTO tag_passaro (nome, id) VALUES ("canario",1),("beija-flor",2),("pas2",2),("sabia",3);
 
-INSERT INTO usuario_viu (email,id,so,ip,browser) VALUES ("manuemmail",1,"WINDOWS","192.168.1.1","Internet Explorer");
-
-INSERT INTO usuario_viu (email,id,so,ip,browser) VALUES ("decoejz",2,"WINDOWS","192.168.1.1","Internet Explorer");
-
-INSERT INTO usuario_viu (email,id,so,ip,browser) VALUES ("manuemmail",2,"WINDOWS","192.168.1.1","Internet Explorer");
+INSERT INTO tag_usuario (email, id) VALUES ("decoejz",1),("use1@mail.com",2),("decoejz",3),("use2@mail.com",1);
